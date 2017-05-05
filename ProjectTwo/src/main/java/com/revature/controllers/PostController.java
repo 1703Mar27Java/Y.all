9e1ecd8e714be8c18aa80931e36d888a7dabe08a
Mehrab Rahman <mehrabmrahman@gmail.com>
2017-05-04 21:00:08 -0400

    thread view

--- ProjectTwo/src/main/java/com/revature/controllers/PostController.java
+++ ProjectTwo/src/main/java/com/revature/controllers/PostController.java
@@ -1,23 +1,40 @@
 package com.revature.controllers;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.revature.beans.*;
+import com.revature.dao.*;
 
 @Controller
 @RequestMapping("/thread")
 public class PostController {
-	@RequestMapping(value="/{threadId}",method=RequestMethod.GET)
+	@RequestMapping(value = "/{threadId}", method = RequestMethod.GET)
 	public String getThread(@PathVariable int threadId, Model m) {
-		m.addAttribute("threadId", threadId);
+		PostDao dao = new PostDaoImpl();
+		try {
+			List<Post> posts = dao.loadAll();
+			List<Post> filtered = new ArrayList<>();
+			for (Post i : posts) {
+				if (i.getParent() == threadId)
+					filtered.add(i);
+			}
+			m.addAttribute("filtered", filtered);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "thread";
 	}
-	
-	@RequestMapping(value="/addPost",method=RequestMethod.GET)
-	public String addPost(Model m){
-		m.addAttribute("post",new Post());
+
+	@RequestMapping(value = "/addPost", method = RequestMethod.GET)
+	public String addPost(Model m) {
+		m.addAttribute("post", new Post());
 		return "thread";
 	}
 
